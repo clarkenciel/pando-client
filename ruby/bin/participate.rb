9f7b4c747afa087b7359b18180a632c56eb49875
clarkenciel <danny@clarkenciel.com>
2016-05-05 09:46:24 -0700

    wrapping up bot script

--- ruby/bin/participate.rb
+++ ruby/bin/participate.rb
@@ -1,10 +1,24 @@
+require 'cgi'
 require_relative '../lib/pando-client.rb'
 
-# p = Modes::Participant.new 'localhost:10001', 'test', 'om'
+# pando
 p = Modes::Participant.new 'clarkenciel.com', 'calarts', 'om'
 
+# oblique strategies
+uri = URI('http://www.oblique-strategies.com')
+h2_regexp = /.*<h2>(.*)<\/h2>/
+tag_open_regexp = /<.*>/
+tag_close_regexp = /<\/.*>/
+
+# times
+times = [5, 10, 20].map { |s| s * 60 }
+
 while true do
-  p.send_message('test hello') if [true,false][rand(0..1)]    
-  sleep 1
+  msg = CGI.unescapeHTML(Net::HTTP.get(uri).
+                          match(h2_regexp)[1].
+                          split(tag_open_regexp,2).
+                          join)
+  p.send_message(msg) if [true,false][rand(0..1)]
+  sleep times[rand(0..times.size-1)]
 end
 
